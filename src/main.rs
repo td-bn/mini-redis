e95aedd25b8b1a347eb8fe3b6ab41d3f21405be6
bnabi <bnabi@protonmail.com>
2023-02-15 12:22:39 +0530

    add shared state

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
+use bytes::Bytes;
 use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         // Second item containts SockAddr: IP and port
         let (socket, _) = listener.accept().await.unwrap();
 
+        // Clone the db 
+        let db = db.clone();
         // New task is spawned, socked is **moved** to the new task and processed there
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     // Read/write redis **frames** instead of byte streams.
     let mut connection = Connection::new(socket);
-    let mut db = HashMap::new();
 
     // Use read_frame to receive a command from the connection
     while let Some(frame) = connection.read_frame().await.unwrap() {
@@ -31,11 +36,14 @@ async fn process(socket: TcpStream) {
         // Respond with an error
         let response = match Command::from_frame(frame).unwrap() {
             Command::Set(cmd) => {
-                // Value is stored as a Vec<u8>
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                // Lock is unlocked on drop
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Command::Get(cmd) => {
+                // Lock is unlocked on drop
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     // Expects type to be Bytes, it implements From<Vec<u8>>
                     Frame::Bulk(value.clone().into())
